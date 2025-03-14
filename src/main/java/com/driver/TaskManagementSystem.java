a307e36ce73beaa27a6de624ec9db4025ef5ce9b
Rohini Patil <rohine452001@gmail.com>
2025-03-14 22:16:04 +0530

    Assignment completed

--- src/main/java/com/driver/TaskManagementSystem.java
+++ src/main/java/com/driver/TaskManagementSystem.java
@@ -12,18 +12,51 @@ public class TaskManagementSystem {
 
 	    public TaskManagementSystem() {
 	        // your code goes here
+			this.tasks = new HashSet<>();
+			this.completedTasks= new ArrayList<>();
 	    }
 
 	    public void addTask(String taskName) {
 	    	 // your code goes here
+			if(tasks.contains(taskName)){
+				System.out.println("Task '" + taskName + "' already exists.");
+			} else {
+				tasks.add(taskName);
+				System.out.println("Task '" + taskName + "' added successfully.");
+			}
 	    }
 
 	    public void markTaskAsCompleted(String taskName) {
 	    	 // your code goes here
+			if(tasks.contains(taskName)){
+				tasks.remove(taskName);
+				completedTasks.add(taskName);
+				System.out.println("Task '" + taskName + "' marked as completed.");
+			} else {
+				System.out.println("Task '" + taskName + "' not found.");
+
+			}
 	    }
 
 	    public void displayAllTasks() {
 	    	 // your code goes here
+			System.out.println("Tasks to be completed:");
+			if (tasks.isEmpty()) {
+				System.out.println("- No pending tasks");
+			} else {
+				for (String task : tasks) {
+					System.out.println("- " + task);
+				}
+			}
+
+			System.out.println("Completed tasks:");
+			if (completedTasks.isEmpty()) {
+				System.out.println("- No completed tasks");
+			} else {
+				for (String task : completedTasks) {
+					System.out.println("- " + task);
+				}
+			}
 	    }
 
 	    public static void main(String[] args) {
